238afa63adb8c5c96d8b34e96abc193c86283fec
liusuxian <382185882@qq.com>
2023-10-10 01:37:14 +0800

    代码提交

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using RectangleApplication;
+
+namespace LearningCSharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Rectangle rectangle = new Rectangle();
+            rectangle.Acceptdetails();
+            rectangle.Display();
+        }
+    }
+}
